f84f9be6fb63c7385c39b17b0732efe8c58f7ef9
Rahul <rahul.islam3@gmail.com>
2025-08-28 22:40:56 -0500

    instalation error fix

--- setup_env.sh
+++ setup_env.sh
@@ -7,11 +7,16 @@ conda activate latentsync
 # Install ffmpeg
 conda install -y -c conda-forge ffmpeg
 
+
 # Python dependencies
 pip install -r requirements.txt
 
 # OpenCV dependencies
-sudo apt -y install libgl1
+# sudo apt -y install libgl1
+conda install -y -c conda-forge libgl
+
+# For GLIBCXX_3.4.32 Error
+conda install -y -c conda-forge libstdcxx-ng
 
 # Download the checkpoints required for inference from HuggingFace
 huggingface-cli download ByteDance/LatentSync-1.6 whisper/tiny.pt --local-dir checkpoints
